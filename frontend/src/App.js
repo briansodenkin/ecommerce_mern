a523be64fcf029029f4ce66d0d0dd26440e6063e
briansodenkin <briansodenkin@gmail.com>
2022-08-06 17:07:42 +0800

    Add frontend dir

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+        <a href="/">
+          CUshop
+        </a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
